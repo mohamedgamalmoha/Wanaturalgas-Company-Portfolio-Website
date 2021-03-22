e105f505c5c384cedff2b3e7c8a36275245be3a0
mido gamal <midogamal77771@gmail.com>
2021-03-23 01:10:24 +0200

    Hiding id from admin page

--- receives/admin.py
+++ receives/admin.py
@@ -4,6 +4,9 @@ from .models import Contact, MainRequests
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
+    fieldsets = (('Main Info', {'fields': ('first_name', 'last_name', 'emil_address', 'phone_number', 'zip_code',
+                                           'Question'
+                                           )}),)
 
     list_display = ['__str__']
 
@@ -16,6 +19,11 @@ class ContactAdmin(admin.ModelAdmin):
 
 @admin.register(MainRequests)
 class MainRequestsAdmin(admin.ModelAdmin):
+    fieldsets = (('Main Info', {'fields': (
+        'request_id', 'q1', 'home_temperature', 'field_of_interest', 'issue_of_concern', 'equipment_age', 'first_name',
+        'last_name', 'email', 'contact_phone', 'cell_phone', 'street_address', 'address_line_2', 'wa', 'city',
+        'zip_code'
+    )}),)
 
     list_display = ['__str__']
 
@@ -24,3 +32,4 @@ class MainRequestsAdmin(admin.ModelAdmin):
 
     def get_readonly_fields(self, request, obj=None):
         return self.fields or [f.name for f in self.model._meta.fields]
+
