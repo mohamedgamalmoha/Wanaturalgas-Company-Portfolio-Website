a16e02e71ef48a86271547a7dd6d34c60a11260f
Abdelrahman Mamdouh <bedoo1234321@gmail.com>
2021-03-23 23:31:55 +0200

    fixed cooling view issues & finished post detail page

--- service/urls.py
+++ service/urls.py
@@ -5,10 +5,10 @@ app_name = 'service'
 
 urlpatterns = [
     path('heating', views.heating_view, name='heating'),
-    path('heating/post/<int:heating_id>/<int:post_id>',
+    path('heating/<int:heating_id>/<int:post_id>',
          views.post_heating_view, name='heating_post'),
 
     path('cooling/', views.cooling_view, name='cooling'),
-    path('cooling/post/<int:id1>/<int:id2>',
+    path('cooling/<int:cooling_id>/<int:post_id>',
          views.post_cooling_view, name='cooling_post'),
 ]
--- service/views.py
+++ service/views.py
@@ -17,6 +17,7 @@ def heating_view(request):
     context = {
         'heating': heating,
         'posts': posts,
+        'active': 'heating'
     }
     return render(request, 'service/heating.html', context)
 
@@ -25,7 +26,7 @@ def cooling_view(request):
     cooling = Cooling.objects.first()
     if cooling:  # will remove this if later as we prevented delete permission
         obj_id = cooling.id
-        content_type = ContentType.objects.get_for_model(Heating)
+        content_type = ContentType.objects.get_for_model(Cooling)
         posts = Post.objects.filter(content_type=content_type,
                                     object_id=obj_id)
     else:
@@ -34,6 +35,7 @@ def cooling_view(request):
     context = {
         'cooling': cooling,
         'posts': posts,
+        'active': 'cooling'
     }
     return render(request, 'service/cooling.html', context)
 
@@ -46,17 +48,19 @@ def post_heating_view(request, heating_id, post_id):
                              object_id=obj_id, id=post_id)
     context = {
         'post': post,
+        'active': 'heating'
     }
     return render(request, 'service/post_heating.html', context)
 
 
-def post_cooling_view(request, heating_id, post_id):
-    cooling = get_object_or_404(Cooling, id=heating_id)
+def post_cooling_view(request, cooling_id, post_id):
+    cooling = get_object_or_404(Cooling, id=cooling_id)
     obj_id = cooling.id
-    content_type = ContentType.objects.get_for_model(Heating)
+    content_type = ContentType.objects.get_for_model(Cooling)
     post = get_object_or_404(Post, content_type=content_type,
                              object_id=obj_id, id=post_id)
     context = {
         'post': post,
+        'active': 'cooling'
     }
     return render(request, 'service/post_cooling.html', context)
